5b840e0aaa99ffc4e11472cb1911a4865429e137
Ethan Samsioe Sarras <108585657+EthanSamsioeSarras@users.noreply.github.com>
2024-06-28 14:36:06 +0200

    Add files via upload

---
+++ FlappyBird/states/TitleScreenState.lua
@@ -0,0 +1,16 @@
+TitleScreenState = Class{__includes = BaseState}
+
+function TitleScreenState:update(dt)
+    if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+        gStateMachine:change('countdown')
+    end
+end
+
+function TitleScreenState:render()
+    love.graphics.setFont(flappyFont)
+    love.graphics.printf('Fifty Bird', 0, 64, VIRTUAL_WIDTH, 'center')
+
+    love.graphics.setFont(mediumFont)
+    love.graphics.printf('Press Enter', 0, 100, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Use Space or Mouse 1', 0, 164, VIRTUAL_WIDTH, 'center')
+end
